1edeae959e96ac2515cf9cc67a3712751c021a0e
Oleg <olegllebrez8@gmail.com>
2021-11-18 01:25:13 +0100

    Feature(compose[minor]): Compose Box
    Configuración de las cajas en compose.

--- app/src/main/java/es/miapp/kami_kaze/MainActivity.kt
+++ app/src/main/java/es/miapp/kami_kaze/MainActivity.kt
@@ -16,10 +16,14 @@ package es.miapp.kami_kaze
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import es.miapp.kami_kaze.ui.theme.KamiKazeTheme
 
@@ -37,15 +41,40 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+/**
+ * Elementos de Compose:
+ *
+ * - Box // Equivalente a FrameLayout
+ * - Row y Row Column // Sustituye al Linearlayout
+ * - LazyRow, LazyColumn y LazyVerticalGrid // Sustituto de RecyclerView
+ * - ConstraintLayout // Dominio por código
+ * - Scaffold // Equivalente a Coordinator Layout (Pero diferente)
+ * - Surface // Parecido al Box pero viene de Material
+ * - Card // Representa un surface pero con elementos modificados
+ */
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(text = "Hello $name!", modifier = modifier)
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    widthDp = 400,
+    heightDp = 200
+)
 @Composable
 fun DefaultPreview() {
     KamiKazeTheme {
-        Greeting("Android")
+        Box(
+            Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Greeting("Android")
+            Greeting(
+                "Android",
+                modifier = Modifier.align(Alignment.BottomEnd)
+            )
+        }
     }
 }
